4f5ae07ab3021a087a70f3d29c25ce44f500805f
Sahil1618 <ss8876584@gmail.com>
2025-12-04 13:13:48 +0530

    Add Proxy and Me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,19 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
